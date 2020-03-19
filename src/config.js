57086360c46e7e79fe817c554826e44270d739c4
sinsys <nicholas.hazel@gmail.com>
2020-03-18 23:58:08 -0700

    Updated config for deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  TEST_DATABASE_URL: 'postgresql://taskmon:test@localhost/taskmon_test',
-  DATABASE_URL: 'postgresql://taskmon:test@localhost/taskmon',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://taskmon:test@localhost/taskmon_test',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://taskmon:test@localhost/taskmon',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
